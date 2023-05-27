ef08c0b0684eef43a13f74a155edbee48ea8c1d6
shtefanika-vadym <vadym.shtefanika@gmail.com>
2023-05-27 08:11:58 +0300

    Updating cron expression

--- src/certificates/certificates.service.ts
+++ src/certificates/certificates.service.ts
@@ -24,7 +24,7 @@ export class CertificatesService {
     @InjectRepository(Certificate) private certificateRepository: Repository<Certificate>,
   ) {}
 
-  @Cron(CronExpression.EVERY_HOUR)
+  @Cron(CronExpression.EVERY_DAY_AT_1AM)
   async notifySecretariesEveryMonth(): Promise<void> {
     const secretaries: User[] = await this.userService.getSecretaries();
     for (const secretary of secretaries) {
